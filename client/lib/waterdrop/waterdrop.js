3c2fe2024669ae3394ba1fe2427f09fdba3ddd89
ghking1 <ghking6@gmail.com>
2023-07-06 17:51:39 +0800

    update to boilerplate 8.0.0

--- client/lib/waterdrop/waterdrop.js
+++ client/lib/waterdrop/waterdrop.js
@@ -1,10 +1,36 @@
+;//seperate with other js file
+
+// Place any helper plugins in here.
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
+
 
 /**************************************************
  *
  *  compatible ajax
  *
  **************************************************/
-;function createXHR()
+function createXHR()
 {
     if (typeof(XMLHttpRequest) != "undefined")
     {
@@ -37,6 +63,7 @@
         throw new Error("No XHR object available.");
     }
 }
+
 
 /**************************************************
  *
